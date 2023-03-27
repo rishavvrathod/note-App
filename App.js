da2bb4a74ae8c3fb1f48b262d6b71d9ffd46e6b8
Rishabh Rathore <rishavvrathod@gmail.com>
2023-03-27 19:04:54 +0530

    Notes-app

--- App.js
+++ App.js
@@ -1,12 +1,22 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import { StatusBar } from 'expo-status-bar';
+import { useEffect, useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Intro from './screens/Intro';
+import NoteScreen from './screens/NoteScreen';
 
 export default function App() {
+  const [user,setUser]=useState({});
+  const findUser = async () => {
+   const result = await AsyncStorage.getItem('user');
+   setUser(JSON.parse(result))
+  }
+
+  useEffect(() => {
+    findUser()
+  },[])
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NoteScreen user={user}/> 
   );
 }
 
---
+++ misc/colors.js
@@ -0,0 +1,6 @@
+export default {
+    DARK : '#lelele',
+    LIGHT : '#FFF',
+    PRIMARY : '#dbb2ff',
+    ERROR: '#ff0000',
+};
